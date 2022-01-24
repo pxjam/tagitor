be3d692d8548de97a7642dcc36357679575ba971
d.udovichenko <d.udovichenko@gmail.com>
2022-01-24 21:32:38 +0300

    fix: rm draggable leavings

--- src/components/Tags.jsx
+++ src/components/Tags.jsx
@@ -1,24 +1,7 @@
-import React, { useEffect, useRef } from 'react'
 import css from '../styles/tags.module.css'
 import { Tag } from './Tag.jsx'
 
 export const Tags = ({ tags, onClickRemove }) => {
-  const positions = useRef({})
-  const positionsRef = positions.current
-
-  const onDrag = (idx, left, top) => {
-    console.log('drag', idx, left, top)
-  }
-
-  const onChildRender = (idx, left, top) => {
-    positionsRef[idx] = { left, top }
-    console.log(positionsRef)
-  }
-
-  useEffect(() => {
-
-  }, [])
-
   return (
     <div className={css.tags}>
       {tags.map((tag, idx) => {
@@ -28,8 +11,6 @@ export const Tags = ({ tags, onClickRemove }) => {
             text={tag}
             onClickRemove={() => onClickRemove(idx)}
             addClass={css.tag}
-            onDrag={(left, top) => onDrag(idx, left, top)}
-            onResize={(left, top) => onChildRender(idx, left, top)}
           />
         )
       })}
